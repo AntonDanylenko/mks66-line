6ef7990419afc08104aa6aa876c35a6b85cd5a5e
AntonDanylenko <adanylenko@stuy.edu>
2019-02-07 11:17:28 -0500

    s

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
+color1 = [ 255, 0, 0 ]
 
 draw_line(250,250,300,500,screen,color)
+plot(screen,color1,300,500)
 draw_line(250,250,500,400,screen,color)
+plot(screen,color1,500,400)
+draw_line(250,250,250,400,screen,color)
+plot(screen,color1,250,400)
+draw_line(250,250,500,250,screen,color)
+plot(screen,color1,500,250)
+#draw_line(250,250,100,350,screen,color)
+#plot(screen,color1,100,350)
+#draw_line(250,250,200,400,screen,color)
+#plot(screen,color1,200,400)
+
 display(screen)
 save_extension(screen, 'img.png')
