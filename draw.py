ce8deae04e979a10190cc186e362a9a86998fa91
AntonDanylenko <adanylenko@stuy.edu>
2019-02-07 12:01:22 -0500

    s

--- draw.py
+++ draw.py
@@ -3,7 +3,7 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
-    if(y1-y0<=x1-x0 and y1-y0>=0):
+    if(y1-y0<=x1-x0 and y1-y0>=0): #250 250 500 400
         d = y1+y1-y0-y0+x0-x1
         while x<=x1:
             plot(screen, color, x, y)
@@ -12,7 +12,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d+=x0+x0-x1-x1
             x+=1
             d+=y1+y1-y0-y0
-    elif (y1-y0>x1-x0 and x1-x0>=0):
+    elif (y1-y0>x1-x0 and x1-x0>=0): #250 250 400 500
         d = y1-y0+x0+x0-x1-x1
         while y<=y1:
             plot(screen, color, x, y)
@@ -20,4 +20,22 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 x+=1
                 d+=y1+y1-y0-y0
             y+=1
+            d+=x0+x0-x1-x1
+    elif (y1-y0>=x0-x1): #250 250 500 100
+        d = y1+y1-y0-y0+x0-x1
+        while x<=x1:
+            plot(screen, color, x, y)
+            if d>0:
+                y-=1
+                d+=x0+x0-x1-x1
+            x+=1
+            d+=y1+y1-y0-y0
+    elif (y1-y0<x0-x1): #250 250 400 0
+        d = y1-y0+x0+x0-x1-x1
+        while y>=y1:
+            plot(screen, color, x, y)
+            if d<0:
+                x+=1
+                d+=y1+y1-y0-y0
+            y-=1
             d+=x0+x0-x1-x1
--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 color1 = [ 255, 0, 0 ]
+color2 = [ 0, 0, 255 ]
 
+#oct 1
 draw_line(250,250,300,500,screen,color)
 plot(screen,color1,300,500)
+#oct 2
 draw_line(250,250,500,400,screen,color)
 plot(screen,color1,500,400)
 draw_line(250,250,250,400,screen,color)
 plot(screen,color1,250,400)
 draw_line(250,250,500,250,screen,color)
 plot(screen,color1,500,250)
+
+#oct 8
+draw_line(250,250,500,100,screen,color2)
+plot(screen,color1,500,100)
+#oct 7
+#draw_line(250,250,400,0,screen,color2)
+#plot(screen,color1,400,0)
 #draw_line(250,250,100,350,screen,color)
 #plot(screen,color1,100,350)
 #draw_line(250,250,200,400,screen,color)
